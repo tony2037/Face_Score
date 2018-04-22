23d0356d2993daed0352ab6119609dbfe4896bde
tony2037 <ztex030640417@gmail.com>
2018-04-22 21:40:48 +0800

    model_1 complete

--- model_1/model_1.py
+++ model_1/model_1.py
@@ -4,20 +4,23 @@ from keras.utils import np_utils
 from keras.models import Sequential  
 from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D  
 import numpy as np  
+import data
 np.random.seed(10)  
   
-"""
+
 # Read image data  
-(X_Train, y_Train), (X_Test, y_Test) = data.load_data()  
+(X_Train, y_Train), (X_Test, y_Test) = data.load_data(x_path="./dataset/x/", y_path="./dataset/y/")  
   
+"""
 # Translation of data  
 X_Train40 = X_Train.reshape(X_Train.shape[0], 28, 28, 1).astype('float32')  
 X_Test40 = X_Test.reshape(X_Test.shape[0], 28, 28, 1).astype('float32')  
 """
-
+print("X_Train shape : {}".format(X_Train.shape))
+print("y_Train shape : {}".format(y_Train.shape))
 
 # define image size (h,w,c)
-image_size = (256,512,3)
+image_size = (256,256,3)
 # Create model  
 model = Sequential()  
 # Create CN layer 1  
@@ -40,23 +43,23 @@ model.add(Conv2D(filters=36,
 model.add(MaxPooling2D(pool_size=(2,2)))  
   
 # Add Dropout layer  
-model.add(Dropout(0.25))  
+#model.add(Dropout(0.25))  
 # Add flatten layer
 model.add(Flatten())  
 
-model.add(Dense(128, activation='relu'))  
-model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu'))
+# Add Dropout layer   
+#model.add(Dropout(0.5))
 
 model.add(Dense(1, activation='relu'))  
 
 model.summary()  
 print("") 
-"""
+
 # 定義訓練方式  
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])  
   
 # 開始訓練  
-train_history = model.fit(x=X_Train40_norm,  
-                          y=y_TrainOneHot, validation_split=0.2,  
-                          epochs=10, batch_size=5, verbose=2)
-"""
+train_history = model.fit(x=X_Train,  
+                          y=y_Train, validation_split=0.2,  
+                          epochs=10, batch_size=1, verbose=2)
